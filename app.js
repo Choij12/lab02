1931f77818413b6315c6c5fea20d44efe0916f10
Jacob Choi <jacob.j.choi@gmail.com>
2021-10-28 18:15:26 -0700

    revised functions on questions

--- app.js
+++ app.js
@@ -13,83 +13,42 @@ function getUserName(){
 
 getUserName();
 
-function numberGame() {
-  let i;
-  let numberPicked, picked;
+function question6(){
   let userAnswerNumber;
-  let answered = true;
 
-  while(answered === true) {
-    for(i = 0; i < 4; i++) {
-      userAnswerNumber = prompt('Pick a number between 1 - 10');
-      userAnswerNumber = parseInt(userAnswerNumber, 10);
+  for(let x = 0; x < 5; x++) {
+    userAnswerNumber = prompt('Try and guess a number between 1 - 10!');
+    userAnswerNumber = parseInt(userAnswerNumber);
 
-      if(userAnswerNumber === 9) {
-        alert('Correct!');
-        score++;
-        numberPicked = false;
-        break;
-      } else if(userAnswerNumber > 9) {
-        alert('Try choosing a lower number');
-        console.log(i);
-      } else if(userAnswerNumber < 8){
-        alert('Try choosing a higher number');
-        console.log(i);
-      } else {
-        alert('I said 1 - 10!!!');
-      }
+    if (userAnswerNumber === 2) {
+      alert('Good guess!');
+      console.log(userAnswerNumber);
+      break;
+    } else if (userAnswerNumber > 2) {
+      alert('Try guessing lower!');
+      console.log(userAnswerNumber);
+    } else if (userAnswerNumber < 2) {
+      alert('Try guesing higher!');
+      console.log(userAnswerNumber);
+    } else {
+      alert('I said 1 - 10');
     }
-    if(i === 4) {
-      alert('You failed at all your attempts.. The number was 9');
-      picked = false;
-      document.querySelector('#picking').textConent = 'You failed at all of your attempts.. The number was 9';
+    if (x === 4) {
+      alert('Sorry, you are out of chances.. the number was 2!');
+      break;
     }
   }
 }
 
-numberGame();
-
-// let userName;
-// let count = 0;
-// let i;
-// let answerOptions = ['yes', 'no'];
-// let questions = ['Would you like to know more about me?','Am I a student at Code Fellows?','Am I a Veteran?','Am I korean?','Do I live in Seattle?'];
-
-// let correctAnswer = ['yes', 'yes' , 'yes,' , 'yes' , 'yes'];
-// let correctAnswerList = ['Of course, you would like to know more about me' , 'Yes, I am a student at Code Fellows.' , 'I am a Veteran.' , 'I am Korean.' , 'I do live in Seattle'];
-
-// let correctAnswerId = ['#me' , '#student' , '#veteran' , '#korean' , '#seattle'];
-// let wrongAnswer = 'Sorry, wrong answer!';
-
-// function allAnswer(){
-//   for(i = 0; i < questions.length; i++) {
-//     let userAnswer = prompt(questions[i]);
-//     let answerLowerCase = userAnswer.toLowerCase();
-
-//     if (answerLowerCase === correctAnswer[i]) {
-//       alert("Correct!");
-//       console.log(count);
-//     } else {
-//       alert(wrongAnswer);
-//       console.log(count);
-//     }
-//   }
-// }
-
-// allAnswer();
-
-// function correctAnswerCount() {
-//   document.getElementById('incorrect').textContent = 'You answered ${count}/5 questions right!';
-// }
-// correctAnswerCount();
+question6();
 
 function favCity() {
   let favoriteCities = ['New York', 'San Deigo', 'Miami'];
 
-  let maxAttempts = 6;
+  let maxTries = 6;
   let gotIt = false;
-  let correctA = true; 
-  for (let count = 1; count <= maxAttempts; count += 1) {
+
+  for (let count = 1; count <= maxTries; count += 1) {
 
     let response = prompt('What are my favorite cities');
 
@@ -97,130 +56,45 @@ function favCity() {
       let fav = favoriteCities[i];
       if (fav === response) {
         alert('You got it right!');
-        correctA = true;
+        gotIt = true;
         break;
       }
     }
 
-    if (correctA) {
+    if (gotIt) {
       break;
     }
   }
-
-  alert(favCities);
+  alert(favCity);
 }
+
 favCity();
 
-// function question7(){
-//   let userGuess = prompt('What are some of my favorite cities?','within the United States');
-//   userGuess = userGuess.toLowerCase();
-//   let cities = ['seattle', 'new york', 'portland', 'los angeles', 'san diego', 'miami'];
-//   let favorite = 'new york';
-//   for(let i = 0; i <=6; i++){
-//     console.log('Guess for the favorite city is ' + userGuess);
-//     if (userGuess === favorite){
-//       alert('Wow, New York is my favorite city!');
-//       count++;
-//       break;
-//     } else if (cities.includes(userGuess) && i !==6){
-//       userGuess = prompt('Not my favorite.. but you are close.');
-//       userGuess = userGuess.toLowerCase();
-//       console.log('Your guess is? ' + userGuess);
-//     } else if (cities.includes(userGuess) && i !==6){
-//       userGuess = prompt('Is that even in the United States?');
-//       console.log('Your guess is? ' + userGuess);
-//     }else{
-//       alert('Sorry, my top favorite city is, New York.. but you could have chosen, seattle', 'portland', 'los angeles', 'san diego', 'miami');
-//     }
-//   }
-// }
+let userName;
+let answerOptions = ['yes', 'no'];
+let questions = ['Would you like to know more about me?','Am I a student at Code Fellows?','Am I a Veteran?','Am I korean?','Do I live in Seattle?'];
 
+let correctAnswer = ['yes', 'yes' , 'yes,' , 'yes' , 'yes'];
+let correctAnswerList = ['Of course, you would like to know more about me' , 'Yes, I am a student at Code Fellows.' , 'I am a Veteran.' , 'I am Korean.' , 'I do live in Seattle'];
 
-//function knowMoreAboutMe(){
-//let userAnswer = prompt('Would you like to know more about me?');
-//console.log('The answer is:' + userAnswer);
-//if(userAnswer === 'yes'){
-//alert('Great! You can start taking a guess at these questions about me.');
-//} else {
-//alert('Too bad.. sorry but here are some questions you can guess about me');
-//}
-//return knowMoreAboutMe;
-//}
+let correctAnswerId = ['#me' , '#student' , '#veteran' , '#korean' , '#seattle'];
+let wrongAnswer = 'Sorry, wrong answer!';
 
-//knowMoreAboutMe();
+function allAnswer(){
+  let i = 0;
+  let count = 0;
+  for(i = 0; i < questions.length; i++) {
+    let userAnswer = prompt(questions[i]);
+    let answerLowerCase = userAnswer.toLowerCase();
 
-//function student(){
-//  let userAnswer = prompt('Am I a student at Code Fellows?');
-//  console.log('The answer is:' + userAnswer);
-//  if(userAnswer === 'yes'){
-//    alert('Correct, I am a student at Code Fellows!');
-//  } else {
-//    alert('Incorrect, I am a student at Code Fellows.');
-//  }
-//  return student;
-//}
+    if (answerLowerCase === correctAnswer[i]) {
+      alert('Correct!');
+      console.log(count);
+    } else {
+      alert(wrongAnswer);
+      console.log(count);
+    }
+  }
+}
 
-//student();
-
-//function veteran(){
-//  let userAnswer = prompt('Am I a Veteran?');
-//  console.log('The answer is:' + userAnswer);
-//  if(userAnswer === 'yes'){
-//    alert('Correct, I am a Veteran!');
-//  } else {
-//    alert('Incorrect, I am actually a Veteran.');
-//  }
-//  return veteran;
-//}
-//veteran();
-
-//function korean(){
-//  let userAnswer = prompt('Am I Korean?');
-//  console.log('The answer is:' + userAnswer);
-//  if(userAnswer === 'yes'){
-//    alert('Correct, I am Korean!');
-//  } else {
-//    alert('Incorrect, I am actually Korean.');
-//  }
-//  return korean;
-//}
-
-//korean();
-
-//function gameOver(){
-//  let userAnswer = prompt('Are you done with these questions?');
-//  console.log('The answer is:' + userAnswer);
-//  if(userAnswer === 'yes'){
-//    alert('It was fun playing with you!');
-//  } else {
-//    alert('Sorry, that was the end. It was fun playing with you!');
-//  }
-//  return gameOver;
-//}
-//gameOver();
-
-
-
-//function security(){
-//  let userAnswer = +prompt('For security, what is the current year?');
-//  let correctAnswer = 2021;
-//  let attempts = 4;
-
-//  for(let i = 0; i < attempts; i++){
-//    console.log(i);
-//    while(userAnswer < 1|| userAnswer > 3000){
-//      userAnswer = prompt('Try again: That is the incorrect year!');
-//    }
-//    if(userAnswer === correctAnswer){
-//      alert('You may now enter this website');
-//      break;
-//    }else if (userAnswer !== correctAnswer){
-//      userAnswer = prompt('Try again: Are we going back in time?');
-//    } else{
-//      alert('You got it wrong!');
-//    }
-//  }
-
-//}
-
-//security();
+allAnswer();
